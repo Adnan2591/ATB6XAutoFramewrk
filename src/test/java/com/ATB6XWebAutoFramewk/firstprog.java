a71bcfe12bdcd7fc9b52ef8cd40141e2e71ec5db
adnan <adnanrahman691@gmail.com>
2024-08-01 00:20:43 +0530

    First commit

--- src/test/java/com/ATB6XWebAutoFramewk/firstprog.java
+++ src/test/java/com/ATB6XWebAutoFramewk/firstprog.java
@@ -1,2 +1,7 @@
-package com.ATB6XWebAutoFramewk;public class firstprog {
+package com.ATB6XWebAutoFramewk;
+
+public class firstprog {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
